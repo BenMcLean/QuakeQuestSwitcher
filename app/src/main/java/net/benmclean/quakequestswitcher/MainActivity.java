47687c6ab16c1ad9a9b28b43eaa88762e765d976
Benjamin McLean <mclean.ben@gmail.com>
2019-06-07 23:19:19 -0500

    trying to ask permission

--- app/src/main/java/net/benmclean/quakequestswitcher/MainActivity.java
+++ app/src/main/java/net/benmclean/quakequestswitcher/MainActivity.java
@@ -1,16 +1,27 @@
 package net.benmclean.quakequestswitcher;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.storage.StorageManager;
 import android.os.storage.StorageVolume;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends FragmentActivity {
+    static final int WRITE_EXST = 0x3;
+    static final int READ_EXST = 0x4;
+    private final int request_code = 61996;
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
@@ -31,9 +42,41 @@ public class MainActivity extends FragmentActivity {
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        
+//        askForPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE,WRITE_EXST);
+        askForPermission(Manifest.permission.READ_EXTERNAL_STORAGE,READ_EXST);
 
-        mAdapter = new RecyclerViewAdapter(this, subjects);
+        String path = "/storage/emulated/0";
+
+        File[] files = new File(path).listFiles();
+
+        if (files == null) throw new IllegalStateException("files is null when path is " + path);
+
+        if (files.length == 0) throw new IllegalStateException("files length is zero when path is " + path);
+
+        String[] listFiles = new String[files.length];
+        for (int x=0; x<listFiles.length; x++)
+            listFiles[x] = files[x].getName();
+
+        mAdapter = new RecyclerViewAdapter(this, listFiles);
         recyclerView.setAdapter(mAdapter);
+    }
+
+    private void askForPermission(String permission, int requestCode) {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, permission) != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, permission)) {
+
+                //This is called if user has denied the permission before
+                //In this case I am just asking the permission again
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission}, requestCode);
+
+            } else {
+
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission}, requestCode);
+            }
+        } else {
+            Toast.makeText(this, "" + permission + " is already granted.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
