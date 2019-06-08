87dcf73d954b3af3f219cd81aae2d6592c0dd3c5
Benjamin McLean <mclean.ben@gmail.com>
2019-06-07 21:13:05 -0500

    Added RecyclerView

--- app/src/main/java/net/benmclean/quakequestswitcher/MainActivity.java
+++ app/src/main/java/net/benmclean/quakequestswitcher/MainActivity.java
@@ -2,15 +2,33 @@ package net.benmclean.quakequestswitcher;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-/*
- * Main Activity class that loads {@link MainFragment}.
- */
 public class MainActivity extends FragmentActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    String[] subjects = {"ANDROID", "PHP", "ASP.NET", "JAVA", "C++"};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.FileRecyclerView);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new RecyclerViewAdapter(this, subjects);
+        recyclerView.setAdapter(mAdapter);
+
     }
 }
